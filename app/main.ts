c179457eef6ec644fd5ce6d6f0b775907cd15742
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-08-04 18:24:53 -0500

    fix css

--- app/main.ts
+++ app/main.ts
@@ -63,8 +63,7 @@ try {
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
     // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-    //app.on('ready', () => setTimeout(createWindow, 400));
-    app.on('ready', createWindow);
+    app.on('ready', () => setTimeout(createWindow, 400));
 
     // Quit when all windows are closed.
     app.on('window-all-closed', () => {
