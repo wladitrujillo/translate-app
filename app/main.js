36d1a62581e4f4aab4304a808ee1ad4137258bb6
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-08-04 14:41:00 -0500

    remove timeout 400 on create window

--- app/main.js
+++ app/main.js
@@ -53,7 +53,8 @@ try {
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
     // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-    electron_1.app.on('ready', () => setTimeout(createWindow, 400));
+    //app.on('ready', () => setTimeout(createWindow, 400));
+    electron_1.app.on('ready', createWindow);
     // Quit when all windows are closed.
     electron_1.app.on('window-all-closed', () => {
         // On OS X it is common for applications and their menu bar
