597247bf52c16970b59c66259e18e57da13c86cc
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-08-14 22:36:40 -0500

    refactor: resource service

--- src/app/core/service/electron/electron.service.ts
+++ src/app/core/service/electron/electron.service.ts
@@ -78,29 +78,10 @@ export class ElectronService {
   }*/
 
 
-  saveResourcesToDisk = (data: Resource[]) => {
-    if (this.isElectron) {
-      this.fs.writeFileSync(`${this.appDataPath}\\${Constants.RESOURCES_FILE_NAME}`,
-        JSON.stringify(data, null, 4),
-        {
-          encoding: "utf8",
-          flag: "w",
-          mode: 0o666
-        });
-    }
-  }
 
-  getResourcesFromDisk = () => {
 
-    if (!this.basePath) return [];
 
-    if (this.isElectron) {
-      let data = this.fs.readFileSync(`${this.appDataPath}\\${Constants.RESOURCES_FILE_NAME}`, 'utf8');
-      return JSON.parse(data);
-    }
-    return [];
-  }
-
+  // Project services
 
   saveProjectToDisk = (data: Project) => {
     if (!this.basePath) return;
@@ -178,6 +159,9 @@ export class ElectronService {
     return {} as Project;
   }
 
+
+  // Dialog services
+
   showOpenDialog = (): Observable<string> => {
     if (this.isElectron) {
       this.ipcRenderer.invoke('showOpenDialog', {
@@ -230,6 +214,85 @@ export class ElectronService {
     }
     return this.subject;
   }
+
+
+  // Resources
+  saveResourcesToDisk = (data: Resource[]) => {
+    if (this.isElectron) {
+      this.fs.writeFileSync(`${this.appDataPath}\\${Constants.RESOURCES_FILE_NAME}`,
+        JSON.stringify(data, null, 4),
+        {
+          encoding: "utf8",
+          flag: "w",
+          mode: 0o666
+        });
+    }
+  }
+
+  getResourcesFromDisk = () => {
+
+    if (!this.basePath) return [];
+
+    if (this.isElectron) {
+      let data = this.fs.readFileSync(`${this.appDataPath}\\${Constants.RESOURCES_FILE_NAME}`, 'utf8');
+      return JSON.parse(data);
+    }
+    return [];
+  }
+  addResource(resource: Resource): void {
+    let found = this.resources.some((r: Resource) => r.id == resource.id);
+    if (found) {
+      throw new Error(`El recurso con identificador ${resource.id} ya existe`);
+    }
+
+    this.resources.push(resource); // add resource to the end of the array
+    this.saveResourcesToDisk(this.resources);
+  }
+
+  getResources(): Resource[] {
+    this.resources = this.getResourcesFromDisk();
+    return this.resources;
+  }
+
+  getResourcesFilterByText(text: string): Resource[] {
+    if (!text) {
+      return this.resources;
+    }
+    return this.resources
+      .filter((resource) => {
+        return resource.id?.includes(text) ||
+          resource.translations?.some((translation) => {
+            return translation.value?.includes(text);
+          });
+      });
+  }
+
+
+  updateResource(resource: Resource): void {
+    let index = this.resources.findIndex((r: Resource) => r.id == resource.id);
+    if (index < 0) return;
+    this.resources[index] = resource; // replace resource at index
+    this.saveResourcesToDisk(this.resources);
+  }
+
+  deleteResource(resourceId: string): void {
+    let index = this.resources.findIndex((r: Resource) => r.id == resourceId);
+    if (index < 0) return;
+    this.resources.splice(index, 1); // remove 1 element from index
+    this.saveResourcesToDisk(this.resources);
+  }
+
+  updateTranslation(
+    resourceId: string,
+    translation: Translation): void {
+    let resource = this.resources.find((r: Resource) => r.id == resourceId);
+    if (!resource) return;
+    let index = resource.translations.findIndex((t: any) => t.locale == translation.locale);
+    if (index < 0) return;
+    resource.translations[index] = translation;
+    this.saveResourcesToDisk(this.resources);
+  }
+
 
 
   get appDataPath(): string {
