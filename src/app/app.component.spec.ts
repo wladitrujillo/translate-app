d508b85bc42c5adecd109051ff5f99f09a558892
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-08-24 14:51:42 -0500

    refactor: name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'translate-app'`, () => {
+  it(`should have as title 'translator-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('translate-app');
+    expect(app.title).toEqual('translator-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('translate-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('translator-app app is running!');
   });
 });
